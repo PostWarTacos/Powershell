6768a961abb7d8d0f1e89501e77b82832ea6fca8
PostWarTacos <mwurtz.soc@gmail.com>
2025-02-22 19:55:28 -0600

    Directory Cleanup

--- Get-ComputerInfo/autoruns.ps1
+++
@@ -1,8 +0,0 @@
-﻿<#
-Using Powershell, find ALL autoruns. Remember that an autorun command can come from different locations
-(ie: it can be in the registry, but not found in the startup folder).
-#>
-
-Get-CimInstance -ClassName Win32_StartupCommand |
-  Select-Object -Property Command, Description, User, Location |
-  Out-GridView
--- Get-ComputerInfo/old_profiles.ps1
+++
@@ -1,8 +0,0 @@
-﻿<#
-Write a script that will provide a list of users that haven't logged on in X number of days.
-#>
-
-$Days = Read-Host "How many days will dictate a stale account?"
-
-Get-childItem -Path ("C:\Users") | ? {$_.Name -notlike "*USAF_Admin*" -and  $_.Name -notlike "*Public*" -and  $_.Name -notlike "*ADMIN*" -and  $_.Name -notlike "*Default*"} | `
-select  Name,LastWriteTime | Where {$_.LastWriteTime -lt $(Get-Date).AddDays(-$Days)}
