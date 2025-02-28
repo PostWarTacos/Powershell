bf98e1ad9d5e5d0f4ad04fea7a7fd2e79dcf166e
PostWarTacos <mwurtz.soc@gmail.com>
2025-02-28 15:32:00 -0600

    Updated Script to push scheduled task correctly.

---
+++ PC-Management-Remote/Get-SiteInfoFromDDSAPI.ps1
@@ -0,0 +1,7 @@
+﻿$myPC = "cshaw002"
+$uri = "https://ssdcorpappsrvt1.dpos.loc/esper/Device/AllStores"
+$header = @{"accept" = "text/plain"}
+$web = Invoke-WebRequest -Uri $uri -Headers $header
+$db = $web.content | ConvertFrom-Json
+$site = $db | select storeNumber,siteCode,ipSubnet,timeZone | where sitecode -eq ($(hostname).substring(1,4))
+$site  
