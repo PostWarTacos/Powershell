2dd1ec037ea8527a8c82f9b3a648ac339fe35721
PostWarTacos <mwurtz.soc@gmail.com>
2025-02-28 15:35:28 -0600

    Updated Get-SiteInfo

--- PC-Management-Remote/Get-SiteInfoFromDDSAPI.ps1
+++ PC-Management-Remote/Get-SiteInfoFromDDSAPI.ps1
@@ -1,4 +1,10 @@
-﻿$myPC = "cshaw002"
+﻿<#
+#   Intent: Pulls computer name and queries db built in SSDS for siteCode, subnet, stNum, and timezone
+#   Author: Matthew Wurtz
+#   Date: 28-Feb-25
+#>
+
+# Meant to run from Collection Commander. Change hostname to redirect to another computer
 $uri = "https://ssdcorpappsrvt1.dpos.loc/esper/Device/AllStores"
 $header = @{"accept" = "text/plain"}
 $web = Invoke-WebRequest -Uri $uri -Headers $header
