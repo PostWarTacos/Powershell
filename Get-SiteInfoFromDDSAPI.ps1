7f302c5506c0a4b4982ae3cee9fdde497ffdbd66
PostWarTacos <mwurtz.soc@gmail.com>
2025-03-20 15:41:53 -0500

    updated

--- Get-SiteInfoFromDDSAPI.ps1
+++ Get-SiteInfoFromDDSAPI.ps1
@@ -4,7 +4,7 @@
 #   Date: 28-Feb-25
 #>
 
-# Meant to run from Collection Commander. Change hostname to redirect to another computer
+
 $uri = "https://ssdcorpappsrvt1.dpos.loc/esper/Device/AllStores"
 $header = @{"accept" = "text/plain"}
 $web = Invoke-WebRequest -Uri $uri -Headers $header
@@ -12,10 +12,10 @@ $db = $web.content | ConvertFrom-Json
 
 $output = @()
 
-$list = get-content C:\users\wurtzmt\Desktop\corrupt.txt
+$list = "ctrip207"
 foreach ( $pc in $list ){
     $site = $db | select storeNumber,siteCode,ipSubnet,timeZone | where sitecode -eq $pc.substring(1,4)
     $output += [PSCustomObject]@{ComputerName=$pc; SiteCode=$site.siteCode; StoreNumber=$site.storeNumber; Timezone=$site.timeZone}
 }
 
-$output | Export-Csv C:\Users\wurtzmt\Desktop\corrupt.csv
+$output #| Export-Csv C:\Users\wurtzmt\Desktop\corrupt.csv
