8c1d8e3c6fb156500997cb23de75ad4daf0ee28a
PostWarTacos <mwurtz.soc@gmail.com>
2025-03-24 15:46:33 -0500

    Initial

---
+++ Modules/Get-FileName.psm1
@@ -0,0 +1,16 @@
+Function Get-FileName() {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $initialDirectory
+    )
+    
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "All files (*.*)| *.*"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+
+}
