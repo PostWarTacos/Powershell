f5441dd654036c3253070ad72fcf0456eeac95bd
postwartacos <mwurtz.soc@gmail.com>
2025-03-27 05:42:47 -0500

    rename

---
+++ Modules/Get-SiteInfoFromDDSAPI.psm1
@@ -0,0 +1,18 @@
+﻿Function Get-FileName() {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]$SiteCode,
+        
+        [Parameter()]
+        [int]$storenumber
+    )
+
+    $uri = "https://ssdcorpappsrvt1.dpos.loc/esper/Device/AllStores"
+    $header = @{"accept" = "text/plain"}
+    $web = Invoke-WebRequest -Uri $uri -Headers $header
+    $db = $web.content | ConvertFrom-Json
+    $site = $db | Select-Object storeNumber,siteCode,ipSubnet,timezone | Where-Object sitecode -eq ($(hostname).substring(1,4))
+    $site
+
+}
