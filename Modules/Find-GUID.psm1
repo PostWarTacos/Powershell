a62092ce44b60f2617c9df89e62af3ba0f91304d
PostWarTacos <mwurtz.soc@gmail.com>
2025-04-11 12:37:47 -0500

    Completed

--- Modules/Find-GUID.psm1
+++ Modules/Find-GUID.psm1
@@ -1,10 +1,12 @@
 function Find-GUID{
     [CmdletBinding()]
     param (
-        [Parameter()]
+        [Parameter(Mandatory)]
         [String]$AppName
     )
     Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* |
         Where-Object DisplayName -match $AppName | Select-Object DisplayName, PSChildName, DisplayVersion
     
 }
+
+Export-ModuleMember Find-GUID
