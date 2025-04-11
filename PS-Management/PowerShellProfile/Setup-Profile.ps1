a3d7218ada6068ac75945e3967ef93d68120ad6f
PostWarTacos <mwurtz.soc@gmail.com>
2025-04-11 13:02:54 -0500

    updates to filepaths

--- PS-Management/PowerShellProfile/Setup-Profile.ps1
+++ PS-Management/PowerShellProfile/Setup-Profile.ps1
@@ -1,4 +1,11 @@
-$SharedProfilePath = "$HOME\Documents\PowerShell\MyUnifiedProfile.ps1"
+If ( whoami -like "*wurtzmt*" ){
+    $user = "C:\users\wurtzmt"
+} 
+Else {
+    $user = [System.Environment]::GetFolderPath("UserProfile")
+}
+
+$SharedProfilePath = "$user\Documents\Coding\PowerShell\PS-Management\PowerShellProfile\Main Profile\MinimumProfile.ps1"
 $DotSourceLine = ". '$SharedProfilePath'"
 
 $AllProfiles = @(
@@ -9,18 +16,18 @@ $AllProfiles = @(
 )
 
 # Also add ISE specific path
-$ISEProfile = "$HOME\Documents\WindowsPowerShell\Microsoft.PowerShellISE_profile.ps1"
-if (!(Test-Path $ISEProfile)) { New-Item -ItemType File -Path $ISEProfile -Force }
+$ISEProfile = "$user\Documents\WindowsPowerShell\Microsoft.PowerShellISE_profile.ps1"
+if ( -not ( Test-Path $ISEProfile )) { New-Item -ItemType File -Path $ISEProfile -Force }
 $AllProfiles += $ISEProfile
 
-foreach ($profilePath in $AllProfiles | Sort-Object -Unique) {
-    if (!(Test-Path $profilePath)) {
+foreach ( $profilePath in $AllProfiles | Sort-Object -Unique ) {
+    if ( -not ( Test-Path $profilePath )) {
         New-Item -ItemType File -Path $profilePath -Force
     }
 
     $content = Get-Content $profilePath -Raw
 
-    if ($content -notmatch [regex]::Escape($DotSourceLine)) {
+    if ( $content -notmatch [regex]::Escape( $DotSourceLine )) {
         Add-Content -Path $profilePath -Value "`n$DotSourceLine"
         Write-Host "Linked: $profilePath"
     } else {
