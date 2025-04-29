480953975569683a0ad1ae72b056922ea663c9b6
PostWarTacos <mwurtz.soc@gmail.com>
2025-04-29 09:44:43 -0500

    Combine and rename

--- Modules/Find-GUID.psm1
+++ Modules/Find-GUID.psm1
@@ -18,4 +18,24 @@ function Find-GUID{
     }
 }
 
-Export-ModuleMember Find-GUID
+function Find-GUIDinMSI {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$MSIPath
+    )
+
+    $installer = New-Object -ComObject WindowsInstaller.Installer
+    $msi = $installer.OpenDatabase("$MSIPath", 0)
+    $view = $msi.OpenView("SELECT * FROM Property")
+    $view.Execute()
+    while ($record = $view.Fetch()) {
+        $name = $record.StringData(1)
+        $value = $record.StringData(2)
+        if ($name -in "ProductCode", "UpgradeCode", "ProductVersion") {
+            Write-Host "$name = $value"
+        }
+    }
+}
+
+Export-ModuleMember Find-GUID, Find-GUIDinMSI
