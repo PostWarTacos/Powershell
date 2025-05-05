fdc0f86441de5015f4b42e2485fb7c05402a6973
PostWarTacos <mwurtz.soc@gmail.com>
2025-05-05 10:59:27 -0500

    Updates

--- Random-Scripts/Encode-Base64.ps1
+++ Random-Scripts/Encode-Base64.ps1
@@ -1,4 +1,20 @@
-$scriptPath = "C:\Users\wurtzmt\desktop\copyrun.ps1" # Path to your script
+Function Get-FileName() {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$InitialDirectory
+    )
+    
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $InitialDirectory
+    $OpenFileDialog.filter = "All files (*.*)| *.*"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+
+}
+
+$scriptPath = Get-FileName -InitialDirectory C:\Users\wurtzmt\Documents\Coding # Path to your script
 $bytes = [System.Text.Encoding]::Unicode.GetBytes((Get-Content $scriptPath -Raw))
 $encodedCommand = [Convert]::ToBase64String($bytes)
 
