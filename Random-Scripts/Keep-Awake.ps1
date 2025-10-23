77d79f5b47c06207b93e0dfd2c4738dbb5c42af7
PostWarTacos <mwurtz.soc@gmail.com>
2025-10-23 15:34:44 -0500

    changed comment

--- Random-Scripts/Keep-Awake.ps1
+++ Random-Scripts/Keep-Awake.ps1
@@ -2,7 +2,7 @@
 Add-Type -AssemblyName System.Windows.Forms
 
 while ($true) {
-    # Simulate "last input" reset every 4 minutes
+    # Simulate "F15" reset every 4 minutes
     [System.Windows.Forms.Application]::DoEvents()
     [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
     [System.Windows.Forms.SendKeys]::SendWait("{F15}")
